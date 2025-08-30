4306e5efb4d579d5e638a6a876653332fd81d9c7
nfroze <Noahfrost@outlook.com>
2025-08-30 22:59:33 +0100

    Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "slack_webhook_url" {
-  description = "https://hooks.slack.com/services/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"
+  description = "Slack webhook URL"
   type        = string
   sensitive   = true
 }
